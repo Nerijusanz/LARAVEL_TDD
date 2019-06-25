caaf0a05f19c80ca70783bbfe4d0ca74715fa89f
nerijus-ubuntu <nerijusanz@gmail.com>
2019-06-25 18:54:04 +0300

    Book module viewPaths created

--- app/Book.php
+++ app/Book.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $guarded = [];
+
+    public $viewPath='/books';
+
+
+    public function viewPath(){
+
+        return $this->viewPath;
+    }
+
+    public function viewPathById(){
+        return $this->viewPath().'/'.$this->id;
+    }
 }
