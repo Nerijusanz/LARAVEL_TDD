0312d493649e9d672bc860ba61ab974048a446ad
nerijus-ubuntu <nerijusanz@gmail.com>
2019-06-26 10:58:56 +0300

    AuthorManagementTest@testAddAuthor test added

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Author;
+
+class AuthorManagementTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function testAddAuthor()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $data = [
+            'name'=>'Author1',
+            'dob'=>'05/14/1988' //dob=>data of birth
+        ];
+
+        $response = $this->post('/authors',$data);
+
+        $this->assertCount(1,Author::all());
+        
+        //-----TEST REDIRECT-------------- //
+        $author = Author::first();
+        $response->assertRedirect($author->viewPath());
+    }
+
+}
