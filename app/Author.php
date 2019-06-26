fae682a6c8144da9ebb0421fd141b926aa2d639e
nerijus-ubuntu <nerijusanz@gmail.com>
2019-06-26 11:19:02 +0300

    Author model viewPath setups added

--- app/Author.php
+++ app/Author.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     protected $guarded = [];
+
+    public $viewPath='/authors';
+
+
+    public function viewPath(){
+
+        return $this->viewPath;
+    }
+
+    public function viewPathById(){
+        return $this->viewPath().'/'.$this->id;
+    }
 }
