a4dd64f3eb966621de690645b7ba514ea8392c16
nerijus-ubuntu <nerijusanz@gmail.com>
2019-06-26 11:49:29 +0300

    Author model carbon::class date setups

--- app/Author.php
+++ app/Author.php
@@ -2,13 +2,23 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Author extends Model
 {
     protected $guarded = [];
 
     public $viewPath='/authors';
+
+    protected $dates = ['dob']; //dob timestamp format field 
+
+    //note: func() name structure set{FieldName}Attribute(${fieldname});
+    public function setDobAttribute($dob)
+    {
+        $this->attributes['dob'] = Carbon::parse($dob); //parse field into carbon format
+    }
 
 
     public function viewPath(){
